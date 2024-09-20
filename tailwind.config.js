cb4f3dda4e0c4e4623e68c00c684d1ca6841d863
Abuzer Emre Osmanoğlu <aeosmanoglu@gmail.com>
2024-09-20 23:19:52 +0300

    font fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,7 @@ module.exports = {
         'title': ['22px', '26.4px'],
       },
       fontFamily: {
-        'body': ['"Poppins"', 'sans-serif'],
+        'outfit': ['Outfit', 'sans-serif'],
       },
       colors: {
         'slate-300': '#D5E1EF',
