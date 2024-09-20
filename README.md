46ebbb53a7d57d8484c29e321a34191a8b57303c
Abuzer Emre Osmanoğlu <aeosmanoglu@gmail.com>
2024-09-20 23:09:31 +0300

    bootstrap removed tailwind init

--- README.md
+++ README.md
@@ -16,7 +16,7 @@ This is a solution to the [QR code component challenge on Frontend Mentor](https
 
 ## Built with
 
-- [Bootstrap](https://getbootstrap.com/)
+- Tailwind CSS
 
 ## Author
 
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      boxShadow: {
+        'card': '0px 25px 25px 0px rgba(0, 0, 0, 0.0477)',
+      },
+      fontSize: {
+        'body': ['15px', '21px'],
+        'title': ['22px', '26.4px'],
+      },
+      fontFamily: {
+        'body': ['"Poppins"', 'sans-serif'],
+      },
+      colors: {
+        'slate-300': '#D5E1EF',
+        'slate-500': '#68778D',
+        'slate-900': '#1F314F',
+      }
+    },
+  },
+  plugins: [],
+}
